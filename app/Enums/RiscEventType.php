e3d0136d8fca4d6072409528550c5e978b846527
er-ko <roman.kocian@gmail.com>
2026-05-06 12:06:47 +0200

    fix: correct RISC event type URIs for token-revoked and credential-change-required

--- app/Enums/RiscEventType.php
+++ app/Enums/RiscEventType.php
@@ -5,9 +5,9 @@ namespace App\Enums;
 enum RiscEventType: string
 {
     case SessionsRevoked = 'https://schemas.openid.net/secevent/risc/event-type/sessions-revoked';
-    case TokenRevoked = 'https://schemas.openid.net/secevent/risc/event-type/token-revoked';
+    case TokenRevoked = 'https://schemas.openid.net/secevent/oauth/event-type/token-revoked';
     case AccountDisabled = 'https://schemas.openid.net/secevent/risc/event-type/account-disabled';
     case AccountEnabled = 'https://schemas.openid.net/secevent/risc/event-type/account-enabled';
-    case CredentialChangeRequired = 'https://schemas.openid.net/secevent/risc/event-type/credential-change-required';
+    case CredentialChangeRequired = 'https://schemas.openid.net/secevent/risc/event-type/account-credential-change-required';
     case Verification = 'https://schemas.openid.net/secevent/risc/event-type/verification';
 }
